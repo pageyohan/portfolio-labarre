66302d02e27b4065cd3cf8c9b867f6e87c448f02
Yohan Page <yohan.page87@gmail.com>
2024-08-12 01:59:06 +0200

    projets

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -5,6 +5,7 @@ import './ProjectDetail.css';
 const ProjectDetail = () => {
   const { title } = useParams();
   const [project, setProject] = useState(null);
+  const [activeOption, setActiveOption] = useState('couleurs');
 
   useEffect(() => {
     const fetchProject = async () => {
@@ -23,15 +24,90 @@ const ProjectDetail = () => {
 
   return (
     <div className="project-detail">
-      <h2>{project.title}</h2>
-      <img src={project.image} alt={project.title} className="project-detail-image" />
-      <div className="project-detail-box">
-        <p>{project.box1}</p>
-        <p>{project.box2}</p>
+      <div className="project-header">
+        <h2>{project.title}</h2>
+      </div>
+      <div className="project-main-content">
+        <img src={project.image} alt={project.title} className="project-main-image" />
+        <div className="project-description">
+          <p>{project.box1}</p>
+          <p>{project.box2}</p>
+        </div>
       </div>
       <div className="project-box3">
         <p>{project.box3}</p>
       </div>
+      <div className="project-images">
+        <img src={project.imagegauche} alt="Placeholder 1" className="project-sub-image"/>
+        <img src={project.imagedroite} alt="Placeholder 2" className="project-sub-image" />
+      </div>
+      <div className="project-divider-custom"></div>
+
+      <div className="project-options">
+        <button 
+          className={`option-button ${activeOption === 'couleurs' ? 'active' : ''}`} 
+          onClick={() => setActiveOption('couleurs')}
+        >
+          Couleur
+        </button>
+        <button 
+          className={`option-button ${activeOption === 'typographie' ? 'active' : ''}`} 
+          onClick={() => setActiveOption('typographie')}
+        >
+          Typographie
+        </button>
+      </div>
+
+      <div className="option-content">
+        {activeOption === 'couleurs' && (
+          <div className="couleurs-content">
+            {project.colors && project.colors.map((color, index) => {
+              const { R, V, B, C, M, J, N, nomCouleur } = color;
+              const backgroundColor = `rgb(${R}, ${V}, ${B})`;
+              const textColor = (R * 0.299 + V * 0.587 + B * 0.114) > 186 ? 'black' : 'white';
+
+              return (
+                <div key={index} className="color-box-container">
+                  <div 
+                    className="color-box" 
+                    style={{ backgroundColor, color: textColor }}
+                  >
+                    <div className="color-row">
+                      <div className="color-label nom-couleur">{nomCouleur}</div>
+                      <div className="color-column">
+                        <span className="color-label">R: {R}</span>
+                        <span className="color-label">V: {V}</span>
+                        <span className="color-label">B: {B}</span>
+                      </div>
+                      <div className="color-column">
+                        <span className="color-label">C: {C}</span>
+                        <span className="color-label">M: {M}</span>
+                        <span className="color-label">J: {J}</span>
+                        <span className="color-label">N: {N}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+            {/* Afficher les descriptions des couleurs sous les color-box */}
+            <div className="color-description">
+              <p>{project.couleurDescription}</p>
+              <p>{project.couleurDescription2}</p>
+            </div>
+          </div>
+        )}
+        {activeOption === 'typographie' && (
+          <div className="typographie-content">
+            {/* Contenu pour la typographie */}
+          </div>
+        )}
+      </div>
+      {project.projectImage && (
+        <div className="project-large-image">
+          <img src={project.projectImage} alt="Grande Image" />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ProjectItem.css';
 
@@ -7,7 +7,16 @@ const moinsImage = `${process.env.PUBLIC_URL}/images/-.svg`;
 
 const ProjectItem = ({ project }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isOpen) {
+      contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+    } else {
+      contentRef.current.style.maxHeight = '0px';
+    }
+  }, [isOpen]);
 
   const toggleAccordion = () => {
     setIsOpen(!isOpen);
@@ -28,25 +37,26 @@ const ProjectItem = ({ project }) => {
         <img 
           src={isOpen ? moinsImage : plusImage} 
           alt={isOpen ? "Moins" : "Plus"} 
-          className={`project-icon ${isOpen ? 'open' : ''}`} 
+          className="project-icon" 
         />
       </div>
-      {isOpen && (
-        <div className="project-content">
-          <div className="project-tags">
-            {project.tags.map((tag, index) => (
-              <span key={index} className="project-tag">{tag}</span>
-            ))}
-          </div>
-          <div className="project-details">
-            <img src={project.image} alt={project.title} className="project-image" />
-            <div className="project-description">
-              <p>{project.description}</p>
-              <button onClick={handleReadMore} className="read-more-button">En savoir plus...</button>
-            </div>
+      <div 
+        className={`project-content ${isOpen ? 'open' : ''}`}
+        ref={contentRef}
+      >
+        <div className="project-tags">
+          {project.tags.map((tag, index) => (
+            <span key={index} className="project-tag">{tag}</span>
+          ))}
+        </div>
+        <div className="project-details">
+          <img src={project.image} alt={project.title} className="project-image" />
+          <div className="project-description">
+            <p>{project.description}</p>
+            <button onClick={handleReadMore} className="read-more-button">En savoir plus...</button>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
